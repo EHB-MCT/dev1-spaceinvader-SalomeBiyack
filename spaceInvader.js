ffed77353b24c07d0afcc1bc029bee24d6a3cae3
Salome Biyack <salome.biyack@student.ehb.be>
2022-10-11 22:26:04 +0200

    try-out

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,37 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+
+drawRectangle();
+function drawRectangle(){
+
+context.beginPath();
+context.fillStyle="#7FFF00";
+context.rect(5,2,50,50);
+context.fill();
+
+}
+
+drawRectangle();
+context.beginPath();
+context.fillStyle="#7FFF00";
+context.rect(30,40,50,50);
+context.fill();
+
+drawRectangle();
+context.beginPath();
+context.fillStyle="#7FFF00";
+context.rect(100,20,50,50);
+context.fill();
+
+drawRectangle();
+context.beginPath();
+context.fillStyle="#7FFF00";
+context.rect(20,100,50,50);
+context.fill();
+
+drawRectangle();
+context.beginPath();
+
+
