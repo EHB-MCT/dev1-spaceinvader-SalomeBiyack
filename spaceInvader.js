c1f7ec28d0ad06d150aa015d9f3a63ca08d42fd9
Salome Biyack <salome.biyack@student.ehb.be>
2022-10-11 23:18:35 +0200

    try-out2

--- spaceInvader.js
+++ spaceInvader.js
@@ -8,30 +8,37 @@ function drawRectangle(){
 
 context.beginPath();
 context.fillStyle="#7FFF00";
-context.rect(5,2,50,50);
+context.rect(25,25,50,25);
 context.fill();
-
 }
 
 drawRectangle();
 context.beginPath();
 context.fillStyle="#7FFF00";
-context.rect(30,40,50,50);
+context.rect(125,25,50,25);
 context.fill();
 
 drawRectangle();
-context.beginPath();
 context.fillStyle="#7FFF00";
-context.rect(100,20,50,50);
+context.rect(25,75,50,25);
 context.fill();
 
 drawRectangle();
-context.beginPath();
 context.fillStyle="#7FFF00";
-context.rect(20,100,50,50);
+context.rect(25,100,50,25);
 context.fill();
 
 drawRectangle();
-context.beginPath();
+context.fillStyle="#7FFF00";
+context.rect(75,100,50,25);
+context.fill();
 
+drawRectangle();
+context.fillStyle="#7FFF00";
+context.rect(100,100,50,25);
+context.fill();
 
+drawRectangle();
+context.fillStyle="#7FFF00";
+context.rect(125,75,50,25);
+context.fill();
